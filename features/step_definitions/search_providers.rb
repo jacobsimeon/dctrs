1ce5c311dd4b34148d55ceb8e84e313e0792fac5
Jacob Morris <jacob.s.morris@gmail.com>
2012-08-25 12:56:44 -0600

    add search_providers step definitions

---
+++ features/step_definitions/search_providers.rb
@@ -0,0 +1,7 @@
+Given /^I am on the home page$/ do
+  visit root_path
+end
+
+Given /^a provider with last name 'Seuss'$/ do
+  pending # express the regexp above with the code you wish you had
+end
