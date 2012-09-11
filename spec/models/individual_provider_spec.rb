b5fc4c6283fdf955b9fef300c05811da71b1624e
Jacob Morris <jacob.s.morris@gmail.com>
2012-09-10 23:53:35 -0600

    add some tests for InvidualProvider#to_indexed_json

--- spec/models/individual_provider_spec.rb
+++ spec/models/individual_provider_spec.rb
@@ -3,4 +3,27 @@ require 'spec_helper'
 describe IndividualProvider do
   it { should accept_nested_attributes_for(:name) }
   it { should have_one :name }
+  describe '#to_indexed_json' do
+
+    it 'returns a string' do
+      @p = create :individual_provider
+      @p.to_indexed_json.should be_a(String)
+    end
+
+    it 'uses empty specialty when provider has no specialties' do
+      @p = create :individual_provider
+      JSON.parse(@p.to_indexed_json)["specialty"].should == {
+        "name" => nil,
+        "description" => nil
+      }
+    end
+
+    it 'serializes the providers specialty' do
+      @p = create :individual_provider
+      @p.specialties.push(create :ortho)
+      json = JSON.parse(@p.to_indexed_json)
+      json["specialty"]["name"].should == "Orthopaedic Surgery"
+    end
+
+  end
 end
