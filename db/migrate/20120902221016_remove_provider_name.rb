97726fc2bf1887f4ab145dd800512b5013fac9ea
Jacob Morris <jacob.s.morris@gmail.com>
2012-09-10 20:54:54 -0600

    remove some old migratiosn

--- db/migrate/20120902221016_remove_provider_name.rb
+++
@@ -1,9 +0,0 @@
-class RemoveProviderName < ActiveRecord::Migration
-  def up
-    remove_column :providers, :full_name
-  end
-
-  def down
-    add_column :providers, :full_name, :string
-  end
-end
--- db/migrate/20120902221246_add_providers_type.rb
+++
@@ -1,8 +0,0 @@
-class AddProvidersType < ActiveRecord::Migration
-  def up
-    add_column :providers, :type, :string
-  end
-
-  def down
-  end
-end
