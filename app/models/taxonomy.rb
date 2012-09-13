af0067039b3e7d56bd0ebffc4e5301579a830772
Jacob Morris <jacob.s.morris@gmail.com>
2012-09-12 23:20:26 -0600

    add sub_taxonomies relationship to taxonomy class

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,5 +1,7 @@
 class Taxonomy < ActiveRecord::Base
   attr_accessible :code, :description, :name
-  belongs_to :category, :class_name => "TaxonomyCategory"
-  belongs_to :parent_taxonomy, :class_name => "Taxonomy"
+
+  belongs_to :category, class_name: "TaxonomyCategory"
+  belongs_to :parent_taxonomy, class_name: "Taxonomy"
+  has_many :sub_taxonomies, class_name: "Taxonomy", :as => "parent_taxonomy"
 end
--- spec/models/taxonomy_spec.rb
+++ spec/models/taxonomy_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Taxonomy do
   it { should belong_to(:category).class_name("TaxonomyCategory") }
   it { should belong_to(:parent_taxonomy).class_name("Taxonomy") }
+  it { should have_many(:sub_taxonomies).class_name("Taxonomy") }
 end
