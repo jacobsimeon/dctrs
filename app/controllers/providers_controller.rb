f3dcb0fda75d0788cf7cb9ef178f30ebe27c34e5
Jacob Morris <jacob.s.morris@gmail.com>
2012-11-11 20:58:02 -0700

    dump a provider on details page

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,7 +1,5 @@
 class ProvidersController < ApplicationController
   def show
     @provider = Provider.find params[:id]
-    raise ActionController::RoutingError.new('Not Found') unless @provider
-    raise @provider.to_yaml
   end
 end
