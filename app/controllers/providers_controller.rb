307977d35b08a785b43e092afc056763970d07b4
Jacob Morris <jacob.s.morris@gmail.com>
2012-11-11 22:06:39 -0700

    allow json retreival

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,5 +1,7 @@
 class ProvidersController < ApplicationController
+  respond_to :html, :json
   def show
     @provider = Provider.find params[:id]
+    respond_with @provider
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
+  respond_to :html, :json
   def index
     providers = Provider.search(params["query"], :page => (params[:page] || 1))
     @providers = ProviderDecorator.decorate(providers)
+    respond_with @providers
   end
 end
