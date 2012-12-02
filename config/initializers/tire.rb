f7df1189700eca75f8a3531c0444943ff1739a3e
Jacob Morris <jacob.s.morris@gmail.com>
2012-12-01 17:00:27 -0700

    fix paths

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,4 +1,10 @@
-ENV['ELASTICSEARCH_URL'] = 'http://xq0vkvx0:ido6siqdctum40h5@holly-4358549.us-east-1.bonsai.io'
+
+
+if Rails.env.production?
+  ENV['ELASTICSEARCH_URL'] = 'http://jacob:7dwarves@searc1.dctrs.io:8080'
+else
+  ENV['ELASTICSEARCH_URL'] = 'http://localhost:9200'
+end
 
 if Rails.env.test?
   prefix = "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}_"
